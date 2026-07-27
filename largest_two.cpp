8be5ded0756e3b5bbcc8b09734bddd3426ab320c
morshed23148-cmyk <morshed23148@gmail.com>
2026-07-27 22:23:12 +0600

    Implement program to find the largest of two numbers

---
+++ largest_two.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+int a,b;
+cout<<"enter two number\n";
+cin>>a>>b;
+if(a>b)
+cout<<a<< "is the largest number\n";
+else if(b>a)
+cout<<b<< "is the largest number\n";
+
+else
+cout<< "both number are equal\n";
+
+return 0;
+}
