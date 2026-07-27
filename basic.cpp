e2159f0224b281f78ee57e40183c71bac3422d4f
morshed23148-cmyk <morshed23148@gmail.com>
2026-07-27 21:11:15 +0600

    add, sub mul ,div syntax
    c++

---
+++ basic.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+int a,b;
+cout<<"enter two number";
+cin>>a>>b;
+cout<<"sum="<<a+b<<endl;
+cout<<"sub="<<a-b<<endl;
+cout<<"mul="<<a*b<<endl;
+cout<<"div="<<a/b<<endl;
+return 0;
+}
